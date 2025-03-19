5f07e4670f426f9913009b7d3a2035d026452c83
tomatija <tomazic.matija@gmail.com>
2025-03-19 17:17:18 +0100

    added user role editing on project

--- frontend/src/pages/AssignedUsersList.jsx
+++ frontend/src/pages/AssignedUsersList.jsx
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Card, ListGroup, Button, Modal, Form, Alert } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { addMemberToProject, removeMemberFromProject, clearProjectError, fetchProjectById } from '../store/slices/projectSlice';
+import { addMemberToProject, removeMemberFromProject, updateProjectMemberRole, clearProjectError, fetchProjectById,  } from '../store/slices/projectSlice';
 import { formatErrorMessage } from '../utils/errorUtils';
 
 const AssignedUsersList = ({ projectId }) => {
   const dispatch = useDispatch();
   const { members, error, currentProject } = useSelector(state => state.projects);
   const [showModal, setShowModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState('');
   const [role, setRole] = useState('DEVELOPER');
+  const [editUser, setEditUser] = useState(null);
 
   const handleAddUser = () => {
     if (selectedUser) {
       dispatch(addMemberToProject({ projectId, userId: selectedUser, role }));
       handleCloseModal();
+    }
+  };
+
+  const handleEditUser = () => {
+    if (editUser) {
+      dispatch(updateProjectMemberRole({ projectId, userId: editUser.id, role }));
+      handleCloseEditModal();
     }
   };
 
@@ -24,8 +33,20 @@ const AssignedUsersList = ({ projectId }) => {
     setRole('DEVELOPER');
   };
 
+  const handleCloseEditModal = () => {
+    setShowEditModal(false);
+    setEditUser(null);
+    setRole('DEVELOPER');
+  };
+
   const handleUserDelete = (userId) => {
     dispatch(removeMemberFromProject({ userId, projectId }));
+  };
+
+  const openEditModal = (user) => {
+    setEditUser(user);
+    setRole(user.role);
+    setShowEditModal(true);
   };
 
   return (
@@ -37,6 +58,9 @@ const AssignedUsersList = ({ projectId }) => {
         {currentProject && currentProject.members && currentProject.members.map(user => (
           <ListGroup.Item key={user.id}>
             {user.user.username} - {user.role}
+            <Button variant="outline-primary" onClick={() => openEditModal(user)} style={{ float: 'right', marginLeft: '10px' }}>
+              Edit
+            </Button>
             <Button variant="danger" onClick={() => handleUserDelete(user.id)} style={{ float: 'right' }}>
               Delete
             </Button>
@@ -49,6 +73,7 @@ const AssignedUsersList = ({ projectId }) => {
         </Button>
       </Card.Body>
 
+      {/* Add User Modal */}
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>Add User to Project</Modal.Title>
@@ -85,6 +110,38 @@ const AssignedUsersList = ({ projectId }) => {
           </Button>
           <Button variant="primary" onClick={handleAddUser}>
             Add User
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Edit User Modal */}
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit User Role</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {error && (
+            <Alert variant="danger" onClose={() => dispatch(clearProjectError())} dismissible>
+              {formatErrorMessage(error)}
+            </Alert>
+          )}
+          <Form>
+            <Form.Group controlId="formEditUserRole">
+              <Form.Label>Role</Form.Label>
+              <Form.Control as="select" value={role} onChange={(e) => setRole(e.target.value)}>
+                <option value="DEVELOPER">Developer</option>
+                <option value="SCRUM_MASTER">Scrum Master</option>
+                <option value="PRODUCT_OWNER">Product Owner</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseEditModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleEditUser}>
+            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
