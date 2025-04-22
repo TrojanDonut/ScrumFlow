f18923fc73205310e5a99c12a32c8af64c090d61
TrojanDonut <tevko2.sedmak@gmail.com>
2025-04-22 15:31:42 +0200

    succesfull web port

--- backend/scrum_project/settings.py
+++ backend/scrum_project/settings.py
@@ -163,6 +163,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://neretva.mknz.si",
+    "https://neretva.mknz.si",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
--- docker-start.sh
+++ docker-start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Get the compose file to use - default to regular docker-compose.yml for dev
+COMPOSE_FILE=${1:-docker-compose.yml}
+echo "Using compose file: $COMPOSE_FILE"
+
 # Stop any existing containers
 echo "Stopping any existing containers..."
-docker compose down
+docker compose -f $COMPOSE_FILE down
 
 # Remove any dangling containers
 echo "Removing any dangling containers..."
@@ -10,6 +14,10 @@ docker container prune -f
 
 # Build and start the containers
 echo "Building and starting containers..."
-docker compose up --build
-
-# This script will be terminated when docker-compose is stopped with Ctrl+C 
+if [ "$2" = "--detach" ] || [ "$2" = "-d" ]; then
+    docker compose -f $COMPOSE_FILE up --build -d
+    echo "Containers started in detached mode."
+else
+    docker compose -f $COMPOSE_FILE up --build
+    # This script will be terminated when docker-compose is stopped with Ctrl+C
+fi 
